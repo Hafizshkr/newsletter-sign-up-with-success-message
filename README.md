814b2d39bf42baa8f63cdce6767cb2d0980c9b08
Hafizshkr <103745630+Hafizshkr@users.noreply.github.com>
2023-09-27 09:00:25 +0800

    newsletter challenge

---
+++ README.md
@@ -0,0 +1,68 @@
+# Frontend Mentor - Newsletter sign-up form with success message solution
+
+This is a solution to the [Newsletter sign-up form with success message challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/newsletter-signup-form-with-success-message-3FC1AZbNrv). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+
+### Screenshot
+
+![](./assets/images/ss.jpg)
+![](./assets/images/Screenshot%20hover.jpg.jpg)
+![](./assets/images/Screenshot%20success.jpg.jpg)
+![](./assets/images/ss%20mobile.jpg.jpg)
+![](./assets/images/Screenshot%20success%20mobile.jpg.jpg)
+
+### Links
+
+- Solution URL: [Github](https://github.com/Hafizshkr/newsletter-sign-up-with-success-message)
+- Live Site URL: [Vercel](https://your-live-site-url.com)
+
+
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Javascript
+
+
+### What I learned
+
+What I have learned from this challenge is how to use the Document Object Model (DOM) in JavaScript.
+
+```js
+submitBtn.addEventListener("click", function(event){
+    event.preventDefault();
+
+    if (!fillTheBox()) {
+        return; // If they didn't fill the input, it will block access to the success page.
+    }
+
+    if (!inputBox.validity.valid){
+        errorMsg.style.display = "block";
+        return;
+    }
+    else {
+        subContainer.style.display = "none";
+        successContainer.style.display = "block";
+        let email = inputBox.value;
+
+        paragraph.innerHTML = `A confirmation email has been sent to <strong>${email}</strong>.
+        Please open it and click the button inside to confirm your subscription.`;
+     }
+})
+```
+
---
+++ index.js
@@ -0,0 +1,67 @@
+const inputBox = document.getElementById("input-box");
+const submitBtn = document.getElementById("email-subscription");
+const errorMsg = document.querySelector(".error-message");
+const subContainer = document.querySelector(".container");
+const successContainer = document.querySelector(".success-container");
+
+
+
+function fillTheBox(){
+    if (inputBox.value === "" || inputBox === null){
+        errorMsg.style.display = "block";
+        return false; // return false if indicate they dont fill the email
+    }
+    return true; // return true if the email is fill
+}
+
+// verifying valid email
+
+inputBox.addEventListener("input", function(){
+    if(inputBox.validity.valid){
+        errorMsg.style.display = "none";
+    }
+    
+        
+
+})
+
+/* if they click the button, without fill the email or with invalid email,
+it will force to fill with valid email. */
+
+submitBtn.addEventListener("click", function(event){
+    event.preventDefault();
+
+    if (!fillTheBox()) {
+        return; // if they didnt fill the input, it will block to access the successpage
+    }
+
+    if (!inputBox.validity.valid){
+        errorMsg.style.display = "block";
+        return;
+    }
+    else {
+        subContainer.style.display = "none";
+        successContainer.style.display = "block";
+        let email = inputBox.value;
+
+        paragraph.innerHTML = `A confirmation email has been sent to <strong>${email}</strong>.
+        please open it and click the button inside to confirm your subscription.`
+     }
+     
+})
+
+// dynamicEmail.textContent = inputBox.value;
+// // successpage.html
+// let paragraph = document.getElementById("paragraph");
+//     if (email) {
+//         dynamicEmail.textContent = decodeURIComponent(email);
+//         paragraph.innerHTML = `A confirmation email has been sent to <strong>${email}</strong>.
+//         please open it and click the button inside to confirm your subscription.`
+//     }
+//     else {
+//         dynamicEmail.textContent = "email not provided";
+//         console.log("email didnt pass")
+//     }
+
+/* <p>A confirmation email has been sent to <span class="dynamic-email"></span>.
+            Please open it and click the button inside to confirm your subscription.</p> */
